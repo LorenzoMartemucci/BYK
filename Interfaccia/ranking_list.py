29101fe3a7a7e80b373090690404587a11c5fdf5
Tommaso-Lippolis <Tommaso.Lippolis1@it.ey.com>
2025-06-05 15:39:26 +0200

    aggiornamento classifica
    metodo che prende i punteggi e aggiorna il file scores.csv, gestite eccezioni sullo score utente e aggiustata interfaccia.

--- Interfaccia/ranking_list.py
+++ Interfaccia/ranking_list.py
@@ -7,7 +7,7 @@ def read_scores():
             reader = csv.reader(file)
             for row in reader:
                 if len(row) >= 2:
-                    scores.append({"username": row[0], "punteggio": int(row[1])})
+                    scores.append({"name": row[0], "score": int(row[1])})
     except FileNotFoundError:
         pass  # Il file non esiste ancora, restituisci lista vuota
     return scores
@@ -16,17 +16,11 @@ def write_scores(scores):
     with open("./Progettazione/scores.csv", "w", newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         for entry in scores:
-            writer.writerow([entry["username"], entry["punteggio"]])
+            writer.writerow([entry["name"], entry["score"]])
 
 
 
-updated_score = read_scores()
-print("Classifica :", updated_score)
-new_user= "Nuovo Giocatore"
-new_score= 20
-if new_user.lower() in [entry["username"].lower() for entry in updated_score]:
-    print(f"L'utente '{new_user}' e' gia' presente nella classifica. Inserisci un nome diverso.")
-else: 
-    updated_score.append({"username": new_user, "punteggio": new_score})  # Example of adding a new score
-    updated_score.sort(key=lambda x: x["punteggio"], reverse=True)
-    write_scores(updated_score)
+
+# if new_user.lower() in [entry["name"].lower() for entry in updated_score]:
+#     print(f"L'utente '{new_user}' e' gia' presente nella classifica. Inserisci un nome diverso.")
+
