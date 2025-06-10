497368bdd240159c5a6b78dccbd22804c6575102
LorenzoMartemucci <Lorenzo.Martemucci@it.ey.com>
2025-06-10 17:38:51 +0200

    [WIP] chat tutorial

--- src/interface/chat_tutorial.py
+++ src/interface/chat_tutorial.py
@@ -1,6 +1,7 @@
-from src.interface.style import Style
-from src.interface.chat import Chat
+from interface.style import Style
+from interface.chat import Chat
 import customtkinter as ctk
+from logics.chat_logics import ChatLogics
 
 class ChatTutorial(Chat):
     
@@ -12,28 +13,25 @@ class ChatTutorial(Chat):
         
         self.next_button.configure(command=self.go_to_recap_page)
         # logic field
+        #self.chat_logics = ChatLogics(get_instance_person, self, None) #TODO:Da sistemare 
 
-    # function to handle the specific enter of the prompt
-    
     def go_to_recap_page(self):
-        pass
-        # # Distruggi il frame di input
-        # self.user_input.destroy()
-        # # Riposiziona il bottone al centro della riga
-        # self.next_button.pack_forget()
-        # self.next_button.configure(
-        #     text="Avanti",
-        #     command = self.go_to_next_page, # TODO: Set the command to go to the next page
-        #     state="normal",
-        #     width=150,  # Set a fixed width for the button
-        #     height=90,
-        #     font=Style.WIDGETS_FONT,
-        # )
-        # self.next_button.pack(pady=10, anchor="center")
+        from interface.storytelling_template import StorytellingTemplate
+        recap_page = StorytellingTemplate(self.master)
+        recap_page.pack(fill="both", expand=True)
+        self.destroy()
+
 
     def _on_enter_pressed(self, event):
         if event.state & 0x0001:  # Shift � premuto
             return  # Permetti il normale comportamento di andare a capo
+        
+        if self.get_message_from_textbox() == "test":
+                # Distruggi il frame di input
+                self.user_input.destroy()
+                # Riposiziona il bottone al centro della riga
+                self.next_button.pack(side='left', padx=20, pady=(0, 20), anchor='center')
+
         self.add_message_bubble(self.get_message_from_textbox(), is_user=True)
         self.user_input.delete("1.0", "end")
         return "break"
