642e06a01e92402c5eb39bfcca3a1dc19366fae2
LorenzoMartemucci <Lorenzo.Martemucci@it.ey.com>
2025-06-11 10:52:24 +0200

    add chat final and fix chat tutorial

--- src/interface/recap_page.py
+++ src/interface/recap_page.py
@@ -1,12 +1,12 @@
-from interface.chat_tutorial import ChatTutorial
+from interface.chat_final import ChatFinal
 from interface.storytelling_template import StorytellingTemplate
 
 class RecapPage(StorytellingTemplate):
     def __init__(self, container):
         super().__init__(container, story_text="Inserire il testo recap")
-        #self.next_button.configure(command=self.on_prossimo_button_click)
+        self.next_button.configure(command=self.go_to_chat_final)
 
-    # def on_prossimo_button_click(self): TODO: implementare CHATFINAL
-    #     chat_page_tutorial = ChatTutorial(self.master)
-    #     chat_page_tutorial.pack(fill="both", expand=True)
-    #     self.destroy()
+    def go_to_chat_final(self):
+        chat_final = ChatFinal(self.master)
+        chat_final.pack(fill="both", expand=True)
+        self.destroy()
